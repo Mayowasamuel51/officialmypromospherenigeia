c2f7cbf70198ffbc652aefdc04ef2751ed9e0914
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-09-25 16:10:51 +0100

    new state

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->string('avater')->unique();
+            $table->string('avater')->nullable();
             $table->string("user_social")->nullable();
             $table->string('name')->nullable();
             $table->string('backgroundimage')->nullable();
