ede340107b9131637d7330af4f9218d42d31af2e
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-09-28 14:17:03 +0100

    remove

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -92,7 +92,7 @@ class AuthController extends Controller
     {
         try {
             /** @var \Laravel\Socialite\Contracts\User $socialiteUser */
-            $socialiteUser = Socialite::driver('google')->stateless()->user();
+            $socialiteUser = Socialite::driver('google')->user();
         } catch (ClientException $e) {
             return response()->json(['error' => 'Invalid credentials provided.'], 422);
         }
