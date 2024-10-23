8f37a844b2bab0cfe947a1de2dabb1073e8a4da4
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-10-23 15:48:33 +0100

    dafdaf

--- app/Http/Controllers/PromoTalk.php
+++ app/Http/Controllers/PromoTalk.php
@@ -103,7 +103,7 @@ class PromoTalk extends Controller
                 // ->orWhere('description', 'like', '%lover%')
                 // ->orWhere('description', 'like', '%lady%')
                 // ->orWhere('description', 'like', '%fuck%')
-                // ->inRandomOrder()d
+                // ->inRandomOrder()d   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
                 ->get()
         );
         if ($promotalk) {
