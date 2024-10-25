2fd6068e469add7ff0a8135d0ddad0612ea2bccc
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-10-25 07:51:36 +0100

    added

--- app/Http/Controllers/PromoTalk.php
+++ app/Http/Controllers/PromoTalk.php
@@ -105,7 +105,7 @@ class PromoTalk extends Controller
                 // ->orWhere('description', 'like', '%fuck%')
                 // ->inRandomOrder()d   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
                 // ->get()
-                ->latest()
+                ->latest()->get()
         );
         if ($promotalk) {
             return response()->json([
