38d9acd6f1cd260b91f0244b5dd41b135582241a
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-12-20 19:57:09 +0100

    new call limit

--- app/Http/Controllers/API/HomePageController.php
+++ app/Http/Controllers/API/HomePageController.php
@@ -271,7 +271,7 @@ class HomePageController extends Controller
                 ->latest()
                 ->inRandomOrder()
                 // ->paginate(8)
-                ->limit(4000)
+                ->limit(80)
                 ->get()
         );
 
