5ae58dec63cd98788d3a65af55537dfe820903fc
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-04-20 13:42:48 +0100

    DAFdaF

--- app/Http/Controllers/API/HomePageController.php
+++ app/Http/Controllers/API/HomePageController.php
@@ -300,7 +300,7 @@ class HomePageController extends Controller
                 ->whereIn('itemfree_ads.categories', $categories)
                 ->latest()
                 // ->inRandomOrder()
-                // ->paginate(20)
+                ->paginate(20)
                 
                 ->inRandomOrder()
                 // ->paginate(8)
